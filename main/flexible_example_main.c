3cc849f725d76dcdd46a74e1720bd7566088df56
Probiers Bau <rainer-probiers@wwad.de>
2023-08-18 23:09:58 +0200

    integrate http play

--- main/flexible_example_main.c
+++ main/flexible_example_main.c
@@ -26,10 +26,12 @@
 #include "http_stream.h"
 #include "mp3_decoder.h"
 #include "wav_decoder.h"
+#include "raw_stream.h"
 
 #include "board.h"
 #include "filter_resample.h"
 #include "esp_peripherals.h"
+#include "periph_wifi.h"
 #include "periph_sdcard.h"
 #include "periph_button.h"
 #include "periph_adc_button.h"
@@ -106,6 +108,24 @@ static audio_element_handle_t create_wav_decoder()
     return wav_decoder_init(&wav_cfg);
 }
 
+static audio_element_handle_t create_http_stream(const char *url)
+{
+    http_stream_cfg_t http_cfg = HTTP_STREAM_CFG_DEFAULT();
+    http_cfg.multi_out_num = 1;
+    audio_element_handle_t http_stream = http_stream_init(&http_cfg);
+    mem_assert(http_stream);
+    audio_element_set_uri(http_stream, url);
+    return http_stream;
+}
+
+static audio_element_handle_t create_raw_stream()
+{
+    raw_stream_cfg_t raw_cfg = RAW_STREAM_CFG_DEFAULT();
+    raw_cfg.type = AUDIO_STREAM_WRITER;
+    audio_element_handle_t raw_stream = raw_stream_init(&raw_cfg);
+    return raw_stream;
+}
+
 void flexible_pipeline_playback()
 {
     audio_pipeline_handle_t pipeline_play = NULL;
@@ -115,10 +135,13 @@ void flexible_pipeline_playback()
     ESP_LOGI(TAG, "[ 1 ] Create all audio elements for playback pipeline");
     audio_element_handle_t fatfs_wav_reader_el = create_fatfs_stream(SAVE_FILE_RATE, SAVE_FILE_BITS, SAVE_FILE_CHANNEL, AUDIO_STREAM_READER);
     audio_element_handle_t fatfs_mp3_reader_el = create_fatfs_stream(SAVE_FILE_RATE, SAVE_FILE_BITS, SAVE_FILE_CHANNEL, AUDIO_STREAM_READER);
+    audio_element_handle_t fatfs_wr_stream_el = create_fatfs_stream(SAVE_FILE_RATE, SAVE_FILE_BITS, SAVE_FILE_CHANNEL, AUDIO_STREAM_WRITER);
     audio_element_handle_t mp3_decoder_el = create_mp3_decoder();
     audio_element_handle_t wav_decoder_el = create_wav_decoder();
     audio_element_handle_t filter_upsample_el = create_filter(SAVE_FILE_RATE, SAVE_FILE_CHANNEL, PLAYBACK_RATE, PLAYBACK_CHANNEL);
     audio_element_handle_t i2s_writer_el = create_i2s_stream(PLAYBACK_RATE, PLAYBACK_BITS, PLAYBACK_CHANNEL, AUDIO_STREAM_WRITER);
+    audio_element_handle_t http_stream_el = create_http_stream("http://dl.espressif.com/dl/audio/ff-16b-2c-44100hz.mp3");
+    audio_element_handle_t raw_read_el = create_raw_stream();
 
     ESP_LOGI(TAG, "[ 2 ] Register all audio elements to playback pipeline");
     audio_pipeline_register(pipeline_play, fatfs_wav_reader_el,  "file_wav_reader");
@@ -127,6 +150,7 @@ void flexible_pipeline_playback()
     audio_pipeline_register(pipeline_play, wav_decoder_el,       "wav_decoder");
     audio_pipeline_register(pipeline_play, filter_upsample_el,   "filter_upsample");
     audio_pipeline_register(pipeline_play, i2s_writer_el,        "i2s_writer");
+    audio_pipeline_register(pipeline_play, http_stream_el,       "http_stream");
 
     audio_element_set_uri(fatfs_wav_reader_el, "/sdcard/twinkle_twinkle.wav");
     audio_element_set_uri(fatfs_mp3_reader_el, "/sdcard/muecke_mit_beute.mp3");
@@ -134,16 +158,48 @@ void flexible_pipeline_playback()
     ESP_LOGI(TAG, "[ 3 ] Set up  event listener");
     audio_event_iface_cfg_t evt_cfg = AUDIO_EVENT_IFACE_DEFAULT_CFG();
     audio_event_iface_handle_t evt = audio_event_iface_init(&evt_cfg);
+    audio_pipeline_set_listener(pipeline_play, evt);
     audio_event_iface_set_listener(esp_periph_set_get_event_iface(set), evt);
 
     ESP_LOGI(TAG, "[3.1] Set up  i2s clock");
     i2s_stream_set_clk(i2s_writer_el, PLAYBACK_RATE, PLAYBACK_BITS, PLAYBACK_CHANNEL);
+
+    ESP_LOGI(TAG, "[3.2] Create pipeline to save audio file");
+    audio_pipeline_cfg_t pipeline_save_cfg = DEFAULT_AUDIO_PIPELINE_CONFIG();
+    audio_pipeline_handle_t pipeline_save = audio_pipeline_init(&pipeline_save_cfg);
+
+    ESP_LOGI(TAG, "[3.3] Set up  uri for save file name");
+    audio_element_set_uri(fatfs_wr_stream_el, "/sdcard/test_output.mp3");
+
+    ESP_LOGI(TAG, "[3.4] Register all elements to pipeline_save");
+    audio_pipeline_register(pipeline_save, raw_read_el, "raw");
+    audio_pipeline_register(pipeline_save, fatfs_wr_stream_el, "file");
+
+    ESP_LOGI(TAG, "[3.5] Link elements together raw_stream-->fatfs_stream");
+    const char *link_save[2] = {"raw", "file"};
+    audio_pipeline_link(pipeline_save, &link_save[0], 2);
+
+    ESP_LOGI(TAG, "[3.6] Connect input ringbuffer of pipeline_save to http stream multi output");
+    ringbuf_handle_t rb = audio_element_get_output_ringbuf(raw_read_el);
+    audio_element_set_multi_output_ringbuf(http_stream_el, rb, 0);
+ 
+    ESP_LOGI(TAG, "[4.0] Start and wait for Wi-Fi network");
+    esp_periph_config_t periph_cfg = DEFAULT_ESP_PERIPH_SET_CONFIG();
+    esp_periph_set_handle_t set = esp_periph_set_init(&periph_cfg);
+    periph_wifi_cfg_t wifi_cfg = {
+        .ssid = CONFIG_WIFI_SSID,
+        .password = CONFIG_WIFI_PASSWORD,
+    };
+    esp_periph_handle_t wifi_handle = periph_wifi_init(&wifi_cfg);
+    esp_periph_start(set, wifi_handle);
+    periph_wifi_wait_for_connected(wifi_handle, portMAX_DELAY);
 
     ESP_LOGI(TAG, "[ 4 ] Start playback pipeline");
     bool source_is_mp3_format = false;
     const char *link_tag[4] = {"file_wav_reader", "wav_decoder", "filter_upsample", "i2s_writer"};
     audio_pipeline_link(pipeline_play, &link_tag[0], 4);
     audio_pipeline_run(pipeline_play);
+    audio_pipeline_run(pipeline_save);
     while (1) {
         audio_event_iface_msg_t msg;
         esp_err_t ret = audio_event_iface_listen(evt, &msg, portMAX_DELAY);
@@ -178,7 +234,9 @@ void flexible_pipeline_playback()
     audio_pipeline_terminate(pipeline_play);
     audio_pipeline_unregister_more(pipeline_play, fatfs_wav_reader_el,
                                    fatfs_mp3_reader_el, mp3_decoder_el,
-                                   wav_decoder_el, filter_upsample_el, i2s_writer_el, NULL);
+                                   wav_decoder_el, filter_upsample_el, i2s_writer_el, http_stream_el, NULL);
+
+    audio_pipeline_unregister_more(pipeline_save, raw_read_el, fatfs_wr_stream_el, NULL);
 
     audio_pipeline_remove_listener(pipeline_play);
     esp_periph_set_stop_all(set);
@@ -191,6 +249,12 @@ void flexible_pipeline_playback()
     audio_element_deinit(wav_decoder_el);
     audio_element_deinit(filter_upsample_el);
     audio_element_deinit(i2s_writer_el);
+    audio_element_deinit(http_stream_el);
+    audio_element_deinit(raw_read_el);
+    audio_element_deinit(fatfs_wr_stream_el);
+
+    audio_pipeline_deinit(pipeline_play);
+    audio_pipeline_deinit(pipeline_save);
 }
 
 void app_main(void)
